3ab5a6b70e6322af3de8329b6f788a45f3485146
WyrmwoodSoft <metropolitan.matt@gmail.com>
2018-09-30 15:30:23 -0400

    The Harvest
    Angular 6 side project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GardenComponent } from './garden/garden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GardenComponent
   ],
   imports: [
     BrowserModule
